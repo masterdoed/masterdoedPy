67fc887313f41300982b96b1660f694d608c2cb6
masterdoed <masterdoed@doedspace.de>
2020-12-25 11:47:50 +0100

    additional markets added

--- bittrexPy.py
+++ bittrexPy.py
@@ -1,12 +1,21 @@
 import json
 import sys
+import os
 from bittrex.bittrex import Bittrex
 
 
 my_bittrex = Bittrex(None, None)
-my_markets=["USD-BTC","USD-ETH","BTC-XVG","BTC-NXS"]
+my_markets=["USD-BTC","USD-ETH","BTC-XVG","BTC-NXS", "USD-ADA", "USD-XRP", "BTC-EMC2","BTC-LTC"]
 
 for market in my_markets:
     market_dict=my_bittrex.get_market_summary(market)["result"]
+    
+    if market_dict[0]["MarketName"]=="USD-BTC":
+        if market_dict[0]["Last"] < 23800:
+            os.system("say 'Bitcoin is going down'")
+        elif market_dict[0]["Last"] > 24200:
+            os.system("say 'Bitcoin is above 24200'")
+
+
     print market_dict[0]["TimeStamp"],"   |   ", market_dict[0]["MarketName"],"   |   ",market_dict[0]["Last"]
     print "---------------------------------------------------------------------------------------"
