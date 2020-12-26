04a8ddf54d62d616c4872c35c30cb41d2bdd7ef3
masterdoed <masterdoed@doedspace.de>
2020-12-26 17:34:23 +0100

    limits modified

--- bittrexPy.py
+++ bittrexPy.py
@@ -13,10 +13,12 @@ for market in my_markets:
     if market_dict[0]["MarketName"]=="USD-BTC":
         if market_dict[0]["Last"] < 23800:
             os.system("say 'Bitcoin is going down'")
-        elif market_dict[0]["Last"] > 24500:
-            os.system("say 'Bitcoin is above 24500'")
-        elif market_dict[0]["Last"] > 25000:
-            os.system("say 'Bitcoin is above 25000'")
+        #elif market_dict[0]["Last"] > 24500:
+        #    os.system("say 'Bitcoin is above 24500'")
+        #elif market_dict[0]["Last"] > 25000:
+        #    os.system("say 'Bitcoin is above 25000'")
+        elif market_dict[0]["Last"] > 26000:
+            os.system("say 'Bitcoin is above 26000'")
         elif market_dict[0]["Last"] < 24000:
             os.system("say 'Bitcoin is below 24000'")
 
